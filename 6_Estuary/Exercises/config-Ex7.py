f1658006c33ce1f594c4567ebedc555cd07ace4f
finewilms <fine.wilms@gmail.com>
2020-10-01 19:37:50 +0200

    modified to pnt to Ex6

--- 6_Estuary/Exercises/config-Ex7.py
+++ 6_Estuary/Exercises/config-Ex7.py
@@ -1,6 +1,6 @@
 engine = "dflowfm"
 
-mdufile = "/home/jovyan/notebooks/7_Tidal/Models/ttt.mdu"
-mdufiletemplate = "/home/jovyan/notebooks/7_Tidal/Models/ttt.mdu.template"
-profdeffile = "/home/jovyan/notebooks/7_Tidal/Models/profdef.txt"
-profdeffiletemplate = "/home/jovyan/notebooks/7_Tidal/Models/profdef.txt.template"
+mdufile = "/home/jovyan/notebooks/6_Estuary/Models/ttt.mdu"
+mdufiletemplate = "/home/jovyan/notebooks/6_Estuary/Models/ttt.mdu.template"
+profdeffile = "/home/jovyan/notebooks/6_Estuary/Models/profdef.txt"
+profdeffiletemplate = "/home/jovyan/notebooks/6_Estuary/Models/profdef.txt.template"
